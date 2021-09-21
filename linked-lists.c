d2685a70b5d7e82e73b89a6bb9ccc1c4615fb0da
icarus.gk <icarus.gk@gmail.com>
2021-09-20 21:44:51 -0600

    Add 'create_node' function

--- linked-lists.c
+++ linked-lists.c
@@ -8,7 +8,23 @@ typedef struct node
 }
 node;
 
+node *create_node(int n);
+
 int main(void)
 {
+  
+}
 
+node *create_node(int n)
+{
+  node *new_node = malloc(sizeof(node));
+
+  if (new_node == NULL)
+  {
+    return NULL;
+  }
+  new_node->number = n;
+  new_node->next = NULL;
+  
+  return new_node;
 }
